857cacd79518a35d424c897574ccea467219f773
Naila <165082374+NAILAinfo@users.noreply.github.com>
2025-07-08 22:59:48 +0000

    tic tac toe game

---
+++ Tic_Tac_Toe_Game.py
@@ -0,0 +1,11 @@
+player1 = input("nom du premier joueur")
+player2 = input("nom du deuxieme joueur")
+
+reponse = input(player1 , "choisit-tu de jouer avec X ou bien O ")
+if reponse in ['X', 'O']:
+    break
+print("Entrée invalide. Choisissez X ou bien O.")
+if reponse =='X':
+    print
+print("")
+
