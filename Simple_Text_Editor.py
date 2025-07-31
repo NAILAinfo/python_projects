3884b52d5342e16fcce05699b8171bad27ac8656
Naila <165082374+NAILAinfo@users.noreply.github.com>
2025-07-31 17:32:48 +0000

    modification

--- Simple_Text_Editor.py
+++ Simple_Text_Editor.py
@@ -1,25 +1,13 @@
 file = input("enter the filename to open or create : ")
-with open(file, "r") as f:
-    contenu = f.read()
-
-if contenu == "":
-    print(file + "not found . Creating a new file .")
-    create 
-    print("enter your text (type 'SAVE' on a new line to save and exit) : ")
-
-
-else:
-    print("file found !")
-
 try:
     with open("nouveau.txt", "x") as f:
         print(file + "not found . Creating a new file .")
-        print("enter your text (type 'SAVE' on a new line to save and exit) : ")
+        print("Enter your text (type 'SAVE' on a new line to save and exit) : ")
         while True:
-        p = input("")
-        f.write(p)
-        if phrase == "SAVE":
-            print("File saved")
-            break
+            p = input(" ")
+            if p == "SAVE":
+                print("File saved")
+                break
+            f.write(p +"\n" )
 except FileExistsError:
-    print("file found !")
+    print("File found !")
